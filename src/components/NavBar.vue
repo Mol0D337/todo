3951a20c2101ff46809f91eb0d2f8f50d6a96369
Mol0D337 <zmolodyk@gmail.com>
2020-12-30 15:48:11 +0200

    todo

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,13 +1,40 @@
 <template>
-  $END$
+
+  <nav class="blue darken-3">
+    <div class="nav-wrapper">
+      <router-link
+              to="/"
+              class="brand-logo center"
+      >
+        MOLOD
+      </router-link>
+      <ul id="nav-mobile" class="left hide-on-med-and-down">
+        <router-link
+                tag="li"
+                to="/create"
+                exact
+                style="width: 100px;text-align: center"
+        >
+          <a href="#">Create</a>
+        </router-link>
+        <router-link
+                tag="li"
+                to="/list"
+                style="width: 100px;text-align: center"
+        >
+          <a href="#">List</a>
+        </router-link>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
-  export default {
-    name: "NavBar"
-  }
+  export default {}
 </script>
 
-<style scoped>
+<style lang="sass" scoped>
+  nav
+    padding: 0 2rem
 
 </style>
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -1,13 +1,20 @@
 <template>
-  $END$
+  <div class="bcg"></div>
+
 </template>
 
 <script>
   export default {
-    name: "MainPage"
+    name: "MainPage",
   }
 </script>
 
-<style scoped>
+<style scoped lang="sass">
+  .bcg
+    width: 100vw
+    height: 100vh
+    background: url("../assets/image/bcg-main-page.png") no-repeat center center
+    background-size: cover
+
 
 </style>
