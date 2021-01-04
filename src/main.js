4df5967ee39cb97cdbbddbd707f0dc42a65c3df0
Mol0D337 <zmolodyk@gmail.com>
2021-01-04 13:13:25 +0200

    update login

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import VueScratchable from 'vue-scratchable'
 import 'materialize-css/dist/js/materialize.min'
 
+Vue.use(VueScratchable);
 Vue.config.productionTip = false;
 
 new Vue({
