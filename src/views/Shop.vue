3185c70234442df1dee3ef813d2e341208bcbeb4
Mol0D337 <zmolodyk@gmail.com>
2021-01-06 18:46:02 +0200

    create shop

---
+++ src/views/Shop.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div style="height: 64px; width: 100%; background: #27273C"></div>
+    <ShopCard/>
+  </div>
+</template>
+
+<script>
+  import ShopCard from "../components/Shop/ShopCard";
+  export default {
+    name: "Shop",
+    components: {ShopCard}
+  }
+</script>
+
+<style scoped>
+
+</style>
